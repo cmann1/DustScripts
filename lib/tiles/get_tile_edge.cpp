cb101441f45095c13f809eea867c8667ade2f4d0
C <thecmann1@gmail.com>
2020-06-12 12:06:53 +0200

    Removed incorrect underscores in TIleShape enums in get_tile_shape

--- lib/tiles/get_tile_edge.cpp
+++ lib/tiles/get_tile_edge.cpp
@@ -5,52 +5,52 @@ bool get_tile_top_edge(int shape, float &out x1, float &out y1, float &out x2, f
 	switch(shape)
 	{
 		case TileShape::Full:
-		case TileShape::Big_2:
-		case TileShape::Big_3:
-		case TileShape::Small_3:
-		case TileShape::Big_6:
-		case TileShape::Big_7:
-		case TileShape::Small_7:
-		case TileShape::Half_B:
-		case TileShape::Half_C:
+		case TileShape::Big2:
+		case TileShape::Big3:
+		case TileShape::Small3:
+		case TileShape::Big6:
+		case TileShape::Big7:
+		case TileShape::Small7:
+		case TileShape::HalfB:
+		case TileShape::HalfC:
 			x1 = 0; y1 = 0;
 			x2 = 48; y2 = 0;
 			break;
-		case TileShape::Big_1:
+		case TileShape::Big1:
 			x1 = 0; y1 = 0;
 			x2 = 48; y2 = 24;
 			break;
-		case TileShape::Small_1:
+		case TileShape::Small1:
 			x1 = 0; y1 = 24;
 			x2 = 48; y2 = 48;
 			break;
-		case TileShape::Small_2:
-		case TileShape::Big_8:
+		case TileShape::Small2:
+		case TileShape::Big8:
 			x1 = 0; y1 = 0;
 			x2 = 24; y2 = 0;
 			break;
-		case TileShape::Big_4:
-		case TileShape::Small_6:
+		case TileShape::Big4:
+		case TileShape::Small6:
 			x1 = 24; y1 = 0;
 			x2 = 48; y2 = 0;
 			break;
-		case TileShape::Small_4:
+		case TileShape::Small4:
 			return false;
-		case TileShape::Big_5:
+		case TileShape::Big5:
 			x1 = 0; y1 = 24;
 			x2 = 48; y2 = 0;
 			break;
-		case TileShape::Small_5:
+		case TileShape::Small5:
 			x1 = 0; y1 = 48;
 			x2 = 48; y2 = 24;
 			break;
-		case TileShape::Small_8:
+		case TileShape::Small8:
 			return false;
-		case TileShape::Half_A:
+		case TileShape::HalfA:
 			x1 = 0; y1 = 0;
 			x2 = 48; y2 = 48;
 			break;
-		case TileShape::Half_D:
+		case TileShape::HalfD:
 			x1 = 0; y1 = 48;
 			x2 = 48; y2 = 0;
 			break;
@@ -64,52 +64,52 @@ bool get_tile_bottom_edge(int shape, float &out x1, float &out y1, float &out x2
 	switch(shape)
 	{
 		case TileShape::Full:
-		case TileShape::Big_1:
-		case TileShape::Small_1:
-		case TileShape::Small_5:
-		case TileShape::Big_4:
-		case TileShape::Big_5:
-		case TileShape::Big_8:
-		case TileShape::Half_A:
-		case TileShape::Half_D:
+		case TileShape::Big1:
+		case TileShape::Small1:
+		case TileShape::Small5:
+		case TileShape::Big4:
+		case TileShape::Big5:
+		case TileShape::Big8:
+		case TileShape::HalfA:
+		case TileShape::HalfD:
 			x1 = 48; y1 = 48;
 			x2 = 0; y2 = 48;
 			break;
-		case TileShape::Big_2:
-		case TileShape::Small_8:
+		case TileShape::Big2:
+		case TileShape::Small8:
 			x1 = 24; y1 = 48;
 			x2 = 0; y2 = 48;
 			break;
-		case TileShape::Small_2:
+		case TileShape::Small2:
 			return false;
-		case TileShape::Big_3:
+		case TileShape::Big3:
 			x1 = 48; y1 = 48;
 			x2 = 0; y2 = 24;
 			break;
-		case TileShape::Small_3:
+		case TileShape::Small3:
 			x1 = 48; y1 = 24;
 			x2 = 0; y2 = 0;
 			break;
-		case TileShape::Small_4:
-		case TileShape::Big_6:
+		case TileShape::Small4:
+		case TileShape::Big6:
 			x1 = 48; y1 = 48;
 			x2 = 24; y2 = 48;
 			break;
-		case TileShape::Small_6:
+		case TileShape::Small6:
 			return false;
-		case TileShape::Big_7:
+		case TileShape::Big7:
 			x1 = 48; y1 = 24;
 			x2 = 0; y2 = 48;
 			break;
-		case TileShape::Small_7:
+		case TileShape::Small7:
 			x1 = 48; y1 = 0;
 			x2 = 0; y2 = 24;
 			break;
-		case TileShape::Half_B:
+		case TileShape::HalfB:
 			x1 = 48; y1 = 0;
 			x2 = 0; y2 = 48;
 			break;
-		case TileShape::Half_C:
+		case TileShape::HalfC:
 			x1 = 48; y1 = 48;
 			x2 = 0; y2 = 0;
 			break;
@@ -123,53 +123,53 @@ bool get_tile_left_edge(int shape, float &out x1, float &out y1, float &out x2, 
 	switch(shape)
 	{
 		case TileShape::Full:
-		case TileShape::Big_1:
-		case TileShape::Big_2:
-		case TileShape::Small_2:
-		case TileShape::Big_7:
-		case TileShape::Big_8:
-		case TileShape::Small_8:
-		case TileShape::Half_A:
-		case TileShape::Half_B:
+		case TileShape::Big1:
+		case TileShape::Big2:
+		case TileShape::Small2:
+		case TileShape::Big7:
+		case TileShape::Big8:
+		case TileShape::Small8:
+		case TileShape::HalfA:
+		case TileShape::HalfB:
 			x1 = 0; y1 = 48;
 			x2 = 0; y2 = 0;
 			break;
-		case TileShape::Small_1:
-		case TileShape::Big_5:
+		case TileShape::Small1:
+		case TileShape::Big5:
 			x1 = 0; y1 = 48;
 			x2 = 0; y2 = 24;
 			break;
-		case TileShape::Big_3:
+		case TileShape::Big3:
 			x1 = 0; y1 = 24;
 			x2 = 0; y2 = 0;
 			break;
-		case TileShape::Small_3:
+		case TileShape::Small3:
 			return false;
-		case TileShape::Big_4:
+		case TileShape::Big4:
 			x1 = 0; y1 = 48;
 			x2 = 24; y2 = 0;
 			break;
-		case TileShape::Small_4:
+		case TileShape::Small4:
 			x1 = 24; y1 = 48;
 			x2 = 48; y2 = 0;
 			break;
-		case TileShape::Small_5:
+		case TileShape::Small5:
 			return false;
-		case TileShape::Big_6:
+		case TileShape::Big6:
 			x1 = 24; y1 = 48;
 			x2 = 0; y2 = 0;
 			break;
-		case TileShape::Small_6:
+		case TileShape::Small6:
 			x1 = 48; y1 = 48;
 			x2 = 24; y2 = 0;
 			break;
-		case TileShape::Small_7:
+		case TileShape::Small7:
 			x1 = 0; y1 = 24;
 			x2 = 0; y2 = 0;
 			break;
-		case TileShape::Half_C:
+		case TileShape::HalfC:
 			return false;
-		case TileShape::Half_D:
+		case TileShape::HalfD:
 			return false;
 	}
 	
@@ -181,55 +181,55 @@ bool get_tile_right_edge(int shape, float &out x1, float &out y1, float &out x2,
 	switch(shape)
 	{
 		case TileShape::Full:
-		case TileShape::Big_3:
-		case TileShape::Big_4:
-		case TileShape::Small_4:
-		case TileShape::Big_5:
-		case TileShape::Big_6:
-		case TileShape::Small_6:
-		case TileShape::Half_C:
-		case TileShape::Half_D:
+		case TileShape::Big3:
+		case TileShape::Big4:
+		case TileShape::Small4:
+		case TileShape::Big5:
+		case TileShape::Big6:
+		case TileShape::Small6:
+		case TileShape::HalfC:
+		case TileShape::HalfD:
 			x1 = 48; y1 = 0;
 			x2 = 48; y2 = 48;
 			break;
-		case TileShape::Big_1:
+		case TileShape::Big1:
 			x1 = 48; y1 = 24;
 			x2 = 48; y2 = 48;
 			break;
-		case TileShape::Small_1:
+		case TileShape::Small1:
 			return false;
-		case TileShape::Big_2:
+		case TileShape::Big2:
 			x1 = 48; y1 = 0;
 			x2 = 24; y2 = 48;
 			break;
-		case TileShape::Small_2:
+		case TileShape::Small2:
 			x1 = 24; y1 = 0;
 			x2 = 0; y2 = 48;
 			break;
-		case TileShape::Small_3:
+		case TileShape::Small3:
 			x1 = 48; y1 = 0;
 			x2 = 48; y2 = 24;
 			break;
-		case TileShape::Small_5:
+		case TileShape::Small5:
 			x1 = 48; y1 = 24;
 			x2 = 48; y2 = 48;
 			break;
-		case TileShape::Big_7:
+		case TileShape::Big7:
 			x1 = 48; y1 = 0;
 			x2 = 48; y2 = 24;
 			break;
-		case TileShape::Small_7:
+		case TileShape::Small7:
 			return false;
-		case TileShape::Big_8:
+		case TileShape::Big8:
 			x1 = 24; y1 = 0;
 			x2 = 48; y2 = 48;
 			break;
-		case TileShape::Small_8:
+		case TileShape::Small8:
 			x1 = 0; y1 = 0;
 			x2 = 24; y2 = 48;
 			break;
-		case TileShape::Half_A:
-		case TileShape::Half_B:
+		case TileShape::HalfA:
+		case TileShape::HalfB:
 			return false;
 	}
 	
