022f4e6c99cba7d1b253ae49a403df576e0d2b4f
C <thecmann1@gmail.com>
2020-07-10 14:00:31 +0200

    Added drawing/circle/draw_arc function

--- lib/drawing/circle.cpp
+++ lib/drawing/circle.cpp
@@ -1,3 +1,5 @@
+#include '../std.cpp';
+
 void draw_circle(scene@ g, float x, float y, float radius, uint segments, uint layer, uint sub_layer, float thickness=2, uint colour=0xFFFFFFFF, bool world=true)
 {
 	if(segments < 3) segments = 3;
@@ -77,5 +79,48 @@ void fill_circle_outside(scene@ g, uint layer, uint sub_layer, float x, float y,
 		prev_x = cx;
 		prev_y = cy;
 		angle += step;
+	}
+}
+
+void draw_arc(scene@ g, float x, float y, float size_x, float size_y, float start, float end, uint segments, uint layer, uint sub_layer, float thickness=2, uint colour=0xFFFFFFFF)
+{
+	if(segments < 3)
+		segments = 3;
+	
+	float angle_step = PI2 / segments;
+	float angle_start = start * DEG2RAD;
+	float angle_end = end * DEG2RAD;
+	float angle_delta = angle_end - angle_start;
+	
+	if(angle_delta > PI2)
+	{
+		angle_delta = PI2;
+	}
+	else if(angle_delta < -PI2)
+	{
+		angle_delta = -PI2;
+	}
+	
+	int steps = clamp(ceil_int(abs(angle_delta / angle_step)), 0, segments);
+	
+	float prev_x = x + cos(angle_start) * size_x;
+	float prev_y = y + sin(angle_start) * size_y;
+	
+	for(int i = 1; i < steps; i++)
+	{
+		float t = float(i) / (steps - 1);
+		float angle = angle_start + angle_delta * t;
+		
+		float px = x + cos(angle) * size_x;
+		float py = y + sin(angle) * size_y;
+		
+		g.draw_line(
+			layer, sub_layer, 
+			prev_x, prev_y,
+			px, py,
+			thickness, colour);
+			
+		prev_x = px;
+		prev_y = py;
 	}
 }
