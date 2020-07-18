e4e6d740c8dbe180f9392e4f42777c2105868812
C <thecmann1@gmail.com>
2020-07-18 14:24:06 +0200

    Added font name constants

---
+++ lib/fonts.cpp
@@ -0,0 +1,44 @@
+/**
+ * A list of fonts and sizes, mostly for reference.
+ */
+namespace font
+{
+	
+	const string ENVY_BOLD			= 'envy_bold';
+	const string SANS_BOLD			= 'sans_bold';
+	const string CARACTERES			= 'Caracteres';
+	const string PROXIMANOVA_REG	= 'ProximaNovaReg';
+	const string PROXIMANOVA_THIN	= 'ProximaNovaThin';
+	
+	/*
+	envy_bold
+		20
+	sans_bold
+		20
+		26
+		36
+	Caracteres
+		26
+		36
+		40
+		52
+		64
+		72
+		92
+		140
+	ProximaNovaReg
+		20
+		26
+		36
+		42
+		58
+		72
+		100
+	ProximaNovaThin
+		20
+		26
+		36
+		42
+	*/
+	
+}
