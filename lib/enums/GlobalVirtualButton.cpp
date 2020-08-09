ef13e5e676f2fcae0780ce1e654da140be6df93c
C <thecmann1@gmail.com>
2020-08-09 08:29:46 +0200

    Added Key and Virtual  button enums

---
+++ lib/enums/GlobalVirtualButton.cpp
@@ -0,0 +1,29 @@
+namespace GlobalVirtualButton
+{
+	
+	const int WheelUp = 0;
+	const int WheelDown = 1;
+	const int LeftClick = 2;
+	const int RightClick = 3;
+	const int MiddleClick = 4;
+	const int Escape = 5;
+	const int Return = 6;
+	const int QuickRestart = 7;
+	const int Space = 8;
+	const int Tab = 9;
+	const int Shift = 10;
+	const int Control = 11;
+	const int Alt = 12;
+	const int Back = 13;
+	const int UpArrow = 14;
+	const int DownArrow = 15;
+	const int LeftArrow = 16;
+	const int RightArrow = 17;
+	const int Plus = 18;
+	const int Minus = 19;
+	const int BracketOpen = 20;
+	const int BracketClose = 21;
+	const int Delete = 22;
+	const int EditorAux = 23;
+	
+}
