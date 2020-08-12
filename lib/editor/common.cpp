c89490689ff95ab03ec5a8f84a35bd0d9bcc5ede
C <thecmann1@gmail.com>
2020-08-12 23:14:17 +0200

    Added consume_gvb functions, and string::join for bool

--- lib/editor/common.cpp
+++ lib/editor/common.cpp
@@ -10,7 +10,31 @@ namespace editor_api
 		editor.key_clear_gvb(GVB::MiddleClick);
 		editor.key_clear_gvb(GVB::WheelDown);
 		editor.key_clear_gvb(GVB::WheelUp);
-		editor.key_clear_gvb(GVB::Space);
+		//editor.key_clear_gvb(GVB::Space);
+	}
+	
+	/// Returns true if the given global virtual button is down and then clears it
+	bool consume_gvb(editor_api@ editor, int gvb)
+	{
+		if(editor.key_check_gvb(gvb))
+		{
+			editor.key_clear_gvb(gvb);
+			return true;
+		}
+		
+		return false;
+	}
+	
+	/// Returns true if the given global virtual button is pressed and then clears it
+	bool consume_gvb_press(editor_api@ editor, int gvb)
+	{
+		if(editor.key_check_pressed_gvb(gvb))
+		{
+			editor.key_clear_gvb(gvb);
+			return true;
+		}
+		
+		return false;
 	}
 	
 }
