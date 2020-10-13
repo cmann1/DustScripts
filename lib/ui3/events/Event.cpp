b8e815ccd8608669d384500470db258416a46cd5
C <thecmann1@gmail.com>
2020-10-13 19:55:07 +0200

    UI: Event uses arrays now that angelscript has been patched

--- lib/ui3/events/Event.cpp
+++ lib/ui3/events/Event.cpp
@@ -11,67 +11,24 @@ class Event
 	
 	bool enabled = true;
 	
-//	private array<EventCallback@> listeners;
-	private EventCallback@ listener1;
-	private EventCallback@ listener2;
-	private EventCallback@ listener3;
-	private EventCallback@ listener4;
+	private array<EventCallback@> listeners;
 	private int num_listeners;
 	
 	void on(EventCallback@ callback)
 	{
-		if(@listener1 == @callback || @listener2 == @callback || @listener3 == @callback || @listener4 == @callback)
-			return;
-		
-		switch(num_listeners)
-		{
-			case 0: @listener1 = callback; break;
-			case 1: @listener2 = callback; break;
-			case 2: @listener3 = callback; break;
-			case 3: @listener4 = callback; break;
-			case 4: puts('Max number of listeners reached!'); return;
-		}
-		
-//		listeners.insertLast(callback);
+		listeners.insertLast(callback);
 		num_listeners++;
 	}
 	
 	void off(EventCallback@ callback)
 	{
-		if(@listener4 == @callback)
-		{
-			@listener4 = null;
-			num_listeners--;
-		}
-		else if(@listener3 == @callback)
-		{
-			@listener3 = @listener4;
-			@listener4 = null;
-			num_listeners--;
-		}
-		else if(@listener2 == @callback)
-		{
-			@listener2 = @listener3;
-			@listener3 = @listener4;
-			@listener4 = null;
-			num_listeners--;
-		}
-		else if(@listener1 == @callback)
-		{
-			@listener1 = @listener2;
-			@listener2 = @listener3;
-			@listener3 = @listener4;
-			@listener4 = null;
-			num_listeners--;
-		}
+		int index = listeners.findByRef(callback);
 		
-//		int index = listeners.findByRef(callback);
-//		
-//		if(index >= 0)
-//		{
-//			@listeners[index] = @listeners[--num_listeners];
-//			listeners.resize(num_listeners);
-//		}
+		if(index >= 0)
+		{
+			@listeners[index] = @listeners[--num_listeners];
+			listeners.resize(num_listeners);
+		}
 	}
 	
 	void dispatch(EventInfo@ event)
@@ -79,21 +36,12 @@ class Event
 		if(!enabled)
 			return;
 		
-//		puts(event.type + ' ' + event.target._id);
+//		puts(event.type + (@event.target != null ? ' ' + event.target._id : ''));
 		
-		if(num_listeners >= 1)
-			listener1(event);
-		if(num_listeners >= 2)
-			listener2(event);
-		if(num_listeners >= 3)
-			listener3(event);
-		if(num_listeners >= 4)
-			listener4(event);
-		
-//		for(int i = 0; i < num_listeners; i++)
-//		{
-//			listeners[i](event);
-//		}
+		for(int i = 0; i < num_listeners; i++)
+		{
+			listeners[i](event);
+		}
 	}
 	
 }
