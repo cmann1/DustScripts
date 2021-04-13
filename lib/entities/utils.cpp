cdceb4733882f68f73a6673bc35d16f31ff7518b
C <thecmann1@gmail.com>
2021-04-13 20:58:54 +0200

    Added entities.utils.get_entity_sprite_info function

---
+++ lib/entities/utils.cpp
@@ -0,0 +1,34 @@
+#include '../enums/AttackType.cpp';
+
+void get_entity_sprite_info(
+	controllable@ e, string &out sprite_name, uint &out frame, int &out face)
+{
+	sprites@ spr = e.get_sprites();
+	
+	if(e.attack_state() == AttackType::Idle)
+	{
+		sprite_name = e.sprite_index();
+		frame = uint(e.state_timer());
+		face = e.face();
+	}
+	else
+	{
+		sprite_name = e.attack_sprite_index();
+		frame = uint(e.attack_timer());
+		face = e.attack_face();
+	}
+	
+	// No clue why, but get_animation_length can return 0?
+	// Unable to reproduce
+	// if(spr.get_animation_length(sprite_name) == 0)
+	// {
+	// 	puts('-----------------------------------------------------');
+	// 	puts('--- spr.get_animation_length(sprite_name) == 0');
+	// 	puts(' sprite_name: ' + sprite_name);
+	// 	puts(' frame: ' + frame);
+	// }
+	
+	frame = spr.get_animation_length(sprite_name) != 0
+		? frame % spr.get_animation_length(sprite_name)
+		: 0;
+}
