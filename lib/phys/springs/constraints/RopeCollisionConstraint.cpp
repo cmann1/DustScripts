30cd902e7bf4895bad08da77b458e47e2ed7c56f
C <thecmann1@gmail.com>
2021-06-21 11:31:00 +0200

    Add rope constraint bounds check

--- lib/phys/springs/constraints/RopeCollisionConstraint.cpp
+++ lib/phys/springs/constraints/RopeCollisionConstraint.cpp
@@ -13,6 +13,7 @@ class RopeCollisionConstraint : Constraint
 	
 	void resolve(const float time_scale, const int iteration) override
 	{
+		array<Particle@>@ particles = @this.particles;
 		const int count = int(particles.length);
 		
 		for(int i = 0; i < count - 1; i++)
@@ -22,37 +23,50 @@ class RopeCollisionConstraint : Constraint
 			Particle@ p1 = particles[0];
 			for(int j = 1; j < count; j++)
 			{
+				if(j == i || j - 1 == i)
+				{
+					@p1 = particles[j];
+					continue;
+				}
+				
 				Particle@ p2 = particles[j];
 				
-				if(j != i && j - 1 != i)
+				const float radius = (p1.radius > p2.radius ? p1.radius : p2.radius) + p.radius;
+				
+				if(
+					p.x < (p1.x < p2.x ? p1.x : p2.x) - radius ||
+					p.y < (p1.y < p2.y ? p1.y : p2.y) - radius ||
+					p.x > (p1.x > p2.x ? p1.x : p2.x) + radius ||
+					p.x > (p1.y > p2.y ? p1.y : p2.y) + radius)
 				{
-					line.x1 = p1.x;
-					line.y1 = p1.y;
-					line.x2 = p2.x;
-					line.y2 = p2.y;
-					float x, y;
-					const float t = line.closest_point(p.x, p.y, x, y);
-					const float line_radius = p1.radius + (p2.radius - p1.radius) * t;
-					float dx = p.x - x;
-					float dy = p.y - y;
-					float dist = dx * dx + dy * dy;
-					
-					if(dist < (line_radius + p.radius) * (line_radius + p.radius))
-					{
-						dist = sqrt(dist);
-						float resting_ratio = dist != 0
-							? ((line_radius + p.radius) - dist) / dist
-							: (line_radius + p.radius);
-						resting_ratio = resting_ratio * strength;
-						dx *= resting_ratio;
-						dy *= resting_ratio;
-						p.x += dx;
-						p.y += dy;
-						//p1.x -= dx;
-						//p1.y -= dy;
-						//p2.x -= dx;
-						//p2.y -= dy;
-					}
+					@p1 = p2;
+					continue;
+				}
+				
+				line.x1 = p1.x;
+				line.y1 = p1.y;
+				line.x2 = p2.x;
+				line.y2 = p2.y;
+				float x, y;
+				const float t = line.closest_point(p.x, p.y, x, y);
+				
+				const float line_radius = p1.radius + (p2.radius - p1.radius) * t;
+				const float dx = p.x - x;
+				const float dy = p.y - y;
+				float dist = dx * dx + dy * dy;
+				
+				if(dist < (line_radius + p.radius) * (line_radius + p.radius))
+				{
+					dist = sqrt(dist);
+					const float resting_ratio = (dist != 0
+						? ((line_radius + p.radius) - dist) / dist
+						: (line_radius + p.radius)) * strength;
+					p.x += dx * resting_ratio;
+					p.y += dy * resting_ratio;
+					//p1.x -= dx;
+					//p1.y -= dy;
+					//p2.x -= dx;
+					//p2.y -= dy;
 				}
 				
 				@p1 = p2;
