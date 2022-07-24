088dba50acffbefbed1feddb85c996740609e035
C <thecmann1@gmail.com>
2022-07-24 17:00:51 +0200

    Add strength to spring system position constraint

--- lib/phys/springs/constraints/PositionConstraint.cpp
+++ lib/phys/springs/constraints/PositionConstraint.cpp
@@ -6,18 +6,28 @@ class PositionConstraint : Constraint
 	Particle@ particle;
 	float x;
 	float y;
+	float strength;
 	
-	PositionConstraint(Particle@ particle, const float x, const float y)
+	PositionConstraint(Particle@ particle, const float x, const float y, const float strength = 1)
 	{
 		@this.particle = particle;
 		this.x = x;
 		this.y = y;
+		this.strength = strength;
 	}
 	
 	void resolve(const float time_scale, const int iteration) override
 	{
-		particle.x = x;
-		particle.y = y;
+		if(strength != 1)
+		{
+			particle.x += (x - particle.x) * strength;
+			particle.y += (y - particle.y) * strength;
+		}
+		else
+		{
+			particle.x = x;
+			particle.y = y;
+		}
 	}
 	
 }
