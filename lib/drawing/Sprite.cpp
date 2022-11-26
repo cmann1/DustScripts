2f8bcc9096e5a77bd53b3d3cea4590a2a0da0dde
C <thecmann1@gmail.com>
2022-11-26 22:55:33 +0200

    Add Sprite::get_bounds and get_corners

--- lib/drawing/Sprite.cpp
+++ lib/drawing/Sprite.cpp
@@ -102,13 +102,59 @@ class Sprite
 			scale_x, scale_y, colour);
 	}
 	
-	void real_position(const float x, const float y, const float rotation, float &out out_x, float &out out_y, const float scale_x=1, const float scale_y=1)
+	void real_position(
+		const float x, const float y, const float rotation, float &out out_x, float &out out_y,
+		const float scale_x=1, const float scale_y=1)
 	{
 		float dx = (sprite_offset_x - sprite_width * origin_x) * scale_x;
 		float dy = (sprite_offset_y - sprite_height * origin_y) * scale_y;
 		rotate(dx, dy, rotation * DEG2RAD, dx, dy);
 		out_x = x + dx;
 		out_y = y + dy;
+	}
+	
+	void get_corners(
+		const float x, const float y, const float rotation,
+		const float scale_x, const float scale_y,
+		float &out x1, float &out y1, float &out x2, float &out y2,
+		float &out x3, float &out y3, float &out x4, float &out y4) const
+	{
+		x1 = -sprite_width * origin_x * scale_x;
+		y1 = -sprite_height * origin_y * scale_y;
+		x2 = x1 + sprite_width * scale_x;
+		y2 = y1;
+		x3 = x2;
+		y3 = y2 + sprite_height * scale_y;
+		x4 = x1;
+		y4 = y3;
+		rotate(x1, y1, rotation * DEG2RAD, x1, y1);
+		rotate(x2, y2, rotation * DEG2RAD, x2, y2);
+		rotate(x3, y3, rotation * DEG2RAD, x3, y3);
+		rotate(x4, y4, rotation * DEG2RAD, x4, y4);
+		x1 += x;
+		y1 += y;
+		x2 += x;
+		y2 += y;
+		x3 += x;
+		y3 += y;
+		x4 += x;
+		y4 += y;
+	}
+	
+	void get_bounds(
+		const float x, const float y, const float rotation,
+		const float scale_x, const float scale_y,
+		float &out x1, float &out y1, float &out x2, float &out y2) const
+	{
+		float bx1, by1, bx2, by2, bx3, by3, bx4, by4;
+		get_corners(
+			x, y, rotation,
+			scale_x, scale_y,
+			bx1, by1, bx2, by2, bx3, by3, bx4, by4);
+		x1 = min(min(bx1, bx2), min(bx3, bx4));
+		y1 = min(min(by1, by2), min(by3, by4));
+		x2 = max(max(bx1, bx2), max(bx3, bx4));
+		y2 = max(max(by1, by2), max(by3, by4));
 	}
 	
 }
