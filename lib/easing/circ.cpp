7b49956a44bbd024a06c4ab6b141c4050db47079
C <thecmann1@gmail.com>
2022-12-20 11:16:25 +0200

    Fix circ easing functions

--- lib/easing/circ.cpp
+++ lib/easing/circ.cpp
@@ -3,12 +3,12 @@ float ease_in_circ(const float t)
 	return 1 - sqrt(1 - t * t);
 }
 
-float ease_out_circ(const float t)
+float ease_out_circ(float t)
 {
 	return sqrt(1 - --t * t);
 }
 
-float ease_in_out_circ(const float t)
+float ease_in_out_circ(float t)
 {
 	return ((t *= 2) <= 1 ? 1 - sqrt(1 - t * t) : sqrt(1 - (t -= 2) * t) + 1) / 2;
 }
