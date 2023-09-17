d7cd26816fa053b0bb9b7f947d5dd5914c4f9822
C <thecmann1@gmail.com>
2023-09-17 21:39:36 +0200

    Add struct logic to print_vars

--- lib/utils/print_vars.cpp
+++ lib/utils/print_vars.cpp
@@ -1,106 +1,116 @@
-#include "../enums/VarType.cpp";
+#include '../enums/VarType.cpp';
 
-void print_vars(entity@ e)
+void print_vars(entity@ e, const string &in indent='   ', const int array_max=25, const bool print_type=true)
 {
 	varstruct@ vars = e.vars();
-	puts(e.type_name() + "[" + vars.num_vars() + "]");
+	puts(e.type_name() + '[' + vars.num_vars() + ']');
 	
 	for(uint i = 0; i < vars.num_vars(); i++)
 	{
-//		string name = vars.var_name(i);
-//		varvalue@ value = vars.get_var(i);
-		puts( print_var_value(vars.var_name(i), vars.get_var(i), "   ") );
+		puts(print_var_value(
+			vars.var_name(i), vars.get_var(i), '', indent, array_max, print_type
+		));
 	}
 }
 
-string print_var_value(string name, varvalue@ value, string indent="", int array_max=25, bool print_type=true)
+string print_var_value(
+	string name, varvalue@ value, const string &in start_indent='', const string &in indent='   ',
+	const int array_max=25, const bool print_type=true)
 {
 	const int id = value.type_id();
 	
-	string value_str = "";
+	string value_str = '';
 	
 	if(id == VarType::Bool)
 	{
-		value_str = "" + value.get_bool();
+		value_str += value.get_bool();
 	}
 	else if(id == VarType::Int8)
 	{
-		value_str = "" + value.get_int8();
+		value_str += value.get_int8();
 	}
 	else if(id == VarType::Int16)
 	{
-		value_str = "" + value.get_int16();
+		value_str += value.get_int16();
 	}
 	else if(id == VarType::Int32)
 	{
-		value_str = "" + value.get_int32();
+		value_str += value.get_int32();
 	}
 	else if(id == VarType::Int64)
 	{
-		value_str = "" + value.get_int64();
+		value_str += value.get_int64();
 	}
 	else if(id == VarType::Float)
 	{
-		value_str = "" + value.get_float();
+		value_str += value.get_float();
 	}
 	else if(id == VarType::String)
 	{
-		value_str = "\"" + value.get_string() +"\"";
+		value_str += '\'' + value.get_string() +'\'';
 	}
 	else if(id == VarType::Vec2)
 	{
-		value_str = "<" + value.get_vec2_x() + ", " + value.get_vec2_y() + ">";
+		value_str += '<' + value.get_vec2_x() + ', ' + value.get_vec2_y() + '>';
 	}
 	else if(id == VarType::Array)
 	{
 		vararray@ arr_value = value.get_array();
-		int size = arr_value.size();
-		value_str = var_type_string(arr_value.element_type_id()) + "[" + size + "]";
+		const int size = arr_value.size();
+		const int el_type = arr_value.element_type_id();
+		value_str = var_type_string(el_type) + '[' + size + ']';
+		
+		string val_indent = el_type == VarType::Struct ? start_indent : '';
 		
 		for(int i = 0; i < size; i++)
 		{
 			varvalue@ item = arr_value.at(i);
-			value_str += "\n" + print_var_value(i + "", item, indent + "   ", array_max, false);
+			const string val = 
+			value_str += '\n' + print_var_value(i + '', item, start_indent + indent, indent, array_max, false);
 			
-			if(array_max > 0 and i > array_max and  i + 1 < size)
+			if(array_max > 0 && i >= array_max - 1 &&  i + 1 < size)
 			{
-				value_str += "\n   " + indent + "... " + (size - array_max) + " more";
+				value_str += '\n   ' + start_indent + indent + '... ' + (size - array_max) + ' more';
 				break;
 			}
 		}
 	}
 	else if(id == VarType::Struct)
 	{
-//		value_str = "<" + value.get_vec2_x() + ", " + value.get_vec2_y() + ">";
+		varstruct@ vars = value.get_struct();
+		for(uint i = 0; i < vars.num_vars(); i++)
+		{
+			value_str += '\n' + print_var_value(vars.var_name(i), vars.get_var(i), start_indent + indent, indent, array_max, true);
+		}
 	}
 	
-	return indent + name + (print_type ? "[" + var_type_string(id) + "]" : "") + " = " + value_str;
+	return start_indent + indent + name + (print_type ? '[' + var_type_string(id) + ']' : '') + ' = ' + value_str;
 }
 
 string var_type_string(const int id)
 {
 	if(id == VarType::None)
-		return "None";
+		return 'None';
 	if(id == VarType::Bool)
-		return "Bool";
+		return 'Bool';
 	if(id == VarType::Int8)
-		return "Int8";
+		return 'Int8';
 	if(id == VarType::Int16)
-		return "Int16";
+		return 'Int16';
 	if(id == VarType::Int32)
-		return "Int32";
+		return 'Int32';
 	if(id == VarType::Int64)
-		return "Int64";
+		return 'Int64';
 	if(id == VarType::Float)
-		return "Float";
+		return 'Float';
 	if(id == VarType::String)
-		return "String";
+		return 'String';
 	if(id == VarType::Array)
-		return "Array";
+		return 'Array';
 	if(id == VarType::Struct)
-		return "Struct";
+		return 'Struct';
 	if(id == VarType::Vec2)
-		return "Vec2";
+		return 'Vec2';
 	
-	return "Unknown";
+	return 'Unknown';
 }
