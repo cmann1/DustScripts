4d0aa72af8cd41ca9cdba751c1e01ecc239aa7f8
C <thecmann1@gmail.com>
2023-09-25 22:38:23 +0200

    Add particle sprite names

---
+++ lib/emitters/sprite_names.cpp
@@ -0,0 +1,26 @@
+namespace Emitters
+{
+	
+	const array<string> SpriteNames = {
+		'dust1', 'dust2', 'dust3',
+		'droplet1',
+		'splash1', 'splash2',
+		'mote1', 'mote2',
+		'leafdrift1', 'leafdrift2', 'leafdrift3',
+		'leafspin1', 'leafspin2', 'leafspin3',
+		'glint',
+		'firefly',
+		'foam1', 'foam2',
+		'flow1', 'flow2',
+		'mist1', 'mist2', 'mist3',
+		'littlepuff', 'medpuff', 'bigpuff',
+		'fly1',
+		'trash1', 'trash2', 'trash3', 'trash4', 'trash5', 'trash6', 'trash7',
+		'smallbubble', 'bigbubble',
+		'smallsplash', 'mediumsplash', 'bigsplash',
+		'bigspark', 'mediumspark', 'smallspark',
+		'poly1', 'poly2',
+		'bluefirefly', 'redfirefly', 'neutralfirefly',
+	};
+	
+}
